f1b71ada59e9cf37a44076daafd6e10d381a647b
artem <arcenivashenko@yandex.ru>
2024-11-03 17:44:39 +0300

    comment v main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import java.io.*;
 import java.util.logging.Logger;
 
+
+//sgushgiushgirgiuhgsiougnorno
 public class Main {
     public static void main(String[] args) throws IOException {
         String fileName = "Техника.csv";
